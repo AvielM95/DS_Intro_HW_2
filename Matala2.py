d1279c6c25be3e041d50ce46dc4ff7883a629c1c
AvielM95 <100766755+AvielM95@users.noreply.github.com>
2022-03-07 15:40:41 +0200

    Add files via upload

---
+++ Matala2.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+    
+def reverse(sentence,reverse_word):
+    word = sentence.split()
+    for i in word:
+        if  i == reverse_word:
+            length = len(reverse_word) 
+            reverse_word[::1]
+            print(reverse_word)
+            
+            
+            
+            
+        
+        
+        
+
+reverse('I like apples and I also like bananas', 'also' ) 
+
+#%%
+
+
+
+
